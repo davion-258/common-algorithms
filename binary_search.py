8ba1e4f82460d8f4c732a0180e22b209772fe5f6
davion-258 <hoangnam.itus@gmail.com>
2024-02-29 22:06:31 +0700

    Add binary search

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+# . . . [x] x x x . . . 
+#        ^
+def bisect_left(a, x, lo=0, hi=None):
+    if hi is None:
+        hi = len(a)
+
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x:
+            lo = mid + 1 # elem < x for elem before lo
+        else:
+            hi = mid
+
+    return lo
+
+# . . . x x x x [.] . . 
+#                ^
+def bisect_right(a, x, lo=0, hi=None):
+    if hi is None:
+        hi = len(a)
+
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] <= x:
+            lo = mid + 1 # elem <= x for elem before lo
+        else:
+            hi = mid
+
+    return lo
